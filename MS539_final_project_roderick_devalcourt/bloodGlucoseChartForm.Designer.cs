456fe80fecbacd510138814fa0c6670e03a6c25d
roddeval <roddeval@uat.edu>
2021-02-19 19:14:23 -0600

    blood glucose chart window basic line chart displaying with test data

--- MS539_final_project_roderick_devalcourt/bloodGlucoseChartForm.Designer.cs
+++ MS539_final_project_roderick_devalcourt/bloodGlucoseChartForm.Designer.cs
@@ -28,12 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.addButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(407, 303);
+            this.addButton.Location = new System.Drawing.Point(465, 359);
             this.addButton.Name = "addButton";
             this.addButton.Size = new System.Drawing.Size(75, 23);
             this.addButton.TabIndex = 1;
@@ -41,17 +46,36 @@
             this.addButton.UseVisualStyleBackColor = true;
             this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(528, 275);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
             // bloodGlucoseChartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(552, 394);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.addButton);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "bloodGlucoseChartForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Blood Glucose Chart";
+            this.Load += new System.EventHandler(this.bloodGlucoseChartForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +83,6 @@
         #endregion
 
         private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- MS539_final_project_roderick_devalcourt/bloodGlucoseChartForm.cs
+++ MS539_final_project_roderick_devalcourt/bloodGlucoseChartForm.cs
@@ -5,8 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MS539_final_project_roderick_devalcourt.Entity;
+using MS539_final_project_roderick_devalcourt.Logic;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace MS539_final_project_roderick_devalcourt
 {
@@ -23,5 +27,86 @@ namespace MS539_final_project_roderick_devalcourt
             bloodGlucoseForm dlg = new bloodGlucoseForm();
             dlg.ShowDialog(this);
         }
+
+        private void bloodGlucoseChartForm_Load(object sender, EventArgs e)
+        {
+            LoadChart();
+        }
+
+        private void LoadChart()
+        {
+            StringBuilder stringBuilder = null;
+            string messageText = "";
+            Exception exceptionDetails = null;
+            string path = MS539_final_project_roderick_devalcourt.Properties.Settings.Default.DefaultPath;
+            string fileName = MS539_final_project_roderick_devalcourt.Properties.Settings.Default.FileName;
+            ReadFileLogic readFileLogic = null;
+
+            try
+            {
+                var chart = chart1.ChartAreas[0];
+                chart.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+                chart.AxisX.LabelStyle.Format = "HH:mm";
+                chart.AxisX.LabelStyle.IsEndLabelVisible = true;
+
+                chart1.Series.Clear();
+
+                chart1.Series.Add("BloodGlucose");
+                chart1.Series["BloodGlucose"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series["BloodGlucose"].Color = Color.Green;
+                chart1.Series["BloodGlucose"].XValueType = ChartValueType.Time;
+
+                if ((string.IsNullOrEmpty(path) == false) && (string.IsNullOrEmpty(fileName) == false))
+                {
+                    readFileLogic = new ReadFileLogic();
+                    if (readFileLogic != null)
+                    {
+                        readFileLogic.PathName = path;
+                        readFileLogic.FileName = fileName;
+                        readFileLogic.GetFormattedFileName();
+
+                        if (File.Exists(readFileLogic.FilePathName) == true)
+                        {
+                            readFileLogic.ReadFile();
+
+
+                            foreach (BloodGlucose bloodGlucose in readFileLogic.listBloodGlucose)
+                            {
+
+                                chart1.Series["BloodGlucose"].Points.AddXY(bloodGlucose.TimeRead.ToOADate(), bloodGlucose.MGDL);
+
+
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception exception)
+            {
+                stringBuilder = new StringBuilder();
+                exceptionDetails = exception;
+
+                while (exceptionDetails != null)
+                {
+
+                    messageText = "\r\nMessage: " + exceptionDetails.Message + "\r\nSource: " + exceptionDetails.Source + "\r\nStack Trace: " + exceptionDetails.StackTrace + "\r\n----------\r\n";
+
+                    stringBuilder.Append(messageText);
+
+                    exceptionDetails = exceptionDetails.InnerException;
+
+                }
+
+                messageText = stringBuilder.ToString();
+
+                System.Diagnostics.Debug.WriteLine(messageText);
+
+            }
+
+
+        }
+
     }
 }
